9b7d270afe86050234aa06fa37773bf33b8d2b0e
Matthieu de Hemptinne <matthieu.dehemptinne@gmail.com>
2021-05-25 13:03:01 +0200

    adding parameters

--- ToyDeepCOMBI/parameters_complete.py
+++ ToyDeepCOMBI/parameters_complete.py
@@ -8,3 +8,17 @@ if 'ROOT_DIR' not in os.environ:
     
 if 'PREFIX' not in os.environ:
     os.environ['PREFIX'] = 'default'
+    
+disease_IDs = ['AZ']
+disease = ['Alzheimer']
+ROOT_DIR = os.environ['ROOT_DIR']
+SYN_DATA_DIR = os.path.join(ROOT_DIR, 'data', 'synthetic')
+REAL_DATA_DIR = os.path.join(ROOT_DIR,'data','WTCCC')
+TEST_DIR = os.path.join(ROOT_DIR,'tests')
+IMG_DIR = os.path.join(ROOT_DIR,'img')
+TALOS_OUTPUT_DIR = os.path.join(TEST_DIR,'talos_output')
+PARAMETERS_DIR = os.path.join(TEST_DIR,'parameters')
+SAVED_MODELS_DIR = os.path.join(TEST_DIR,'exported_models')
+TB_DIR = os.path.join(TEST_DIR,'exported_models')
+NUMPY_ARRAYS = os.path.join(ROOT_DIR,'numpy_arrays')
+FINAL_RESULTS_DIR = os.path.join(ROOT_DIR,'experiments','MONTAEZ_final')
