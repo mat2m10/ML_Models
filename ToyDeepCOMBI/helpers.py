5ed1ec3556b6bb3ce9be2b64b6db23b56ed6d95a
Matthieu de Hemptinne <matthieu.dehemptinne@gmail.com>
2021-05-31 20:36:09 +0200

    adding stuff

--- ToyDeepCOMBI/helpers.py
+++ ToyDeepCOMBI/helpers.py
@@ -154,6 +154,7 @@ def generate_syn_genotypes(root_path = SYN_DATA_DIR, n_subjects=syn_n_subjects, 
             pass
         with h5py.File(os.path.join(REAL_DATA_DIR,'AZ','chromo_2.mat'),'r') as f2:
             chrom2_full = np.array(f2.get('X')).T
+            print(chrom2_full)
             chrom2_full = chrom2_full.reshape(chrom2_full.shape[0],-1,3)[:,:,:2]
             chrom2_full = remove_small_frequencies(chrom2_full) # First Method
             chrom2_full = chrom2_full[:, :n_noise_snps]
