65416a9600ca63db4f6e6a7a69d45b83288805df
Matthieu de Hemptinne <matthieu.dehemptinne@gmail.com>
2021-06-03 13:56:02 +0200

    adding commandos

---
+++ ToyDeepCOMBI/tests/test_deepcombi.py
@@ -0,0 +1,7 @@
+class TestDeepCOMBI(object):
+    def test_lrp_svm(self, syn_genomic_data, 
+                     syn_fm, syn_idx, rep, tmp_path, 
+                     syn_true_pvalues):
+        """ Compares efficiency of the combi method with several TTBR
+        """
+        pass
